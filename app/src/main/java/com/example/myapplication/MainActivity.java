e2254761d5fd3b630084417d1aba25508030ec29
mehedi-hasan <mehediinform@gmail.com>
2024-01-10 04:28:08 +0600

    2nd Commit VideoView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.videoView_Id);
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.amaro_chilo_sob_ekadin);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
+
     }
 }
